a2a35135f654e595d5246a721c61d2cb879b449f
Andy Grant <andrew.grant@codecomputerlove.com>
2018-11-01 14:12:09 +0000

    Initial commit

---
+++ tests/SanityTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
